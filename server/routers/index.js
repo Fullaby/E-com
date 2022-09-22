3895a26df1c32a28bd224e7fb8004632e051a648
steven-leonardo <stevenleonardolim@gmail.com>
2022-09-23 00:51:07 +0700

    router connection

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const userRouter= require('./user');
+const cartRouter=require('./cart');
+const productRouter= require('./product');
+const transactionRouter= require('./transactions');
 
 router.use('/user',userRouter)
 router.use('/cart', cartRouter)
 router.use('/product', productRouter)
-router.use('/transactions', transactionRouter)
+router.use('/transaction', transactionRouter)
 
 
 module.exports=router
---
+++ server/routers/product.js
@@ -0,0 +1,5 @@
+const router= require('express').Router();
+const Controller= require('../controllers/product');
+
+
+module.exports= router
