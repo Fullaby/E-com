8da23789e8249959840238a173253805e177b077
steven-leonardo <stevenleonardolim@gmail.com>
2022-09-26 01:36:48 +0700

    handle error on product entity

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,14 +1,22 @@
 const errorHandler= async(error,req,res,next)=>{
     console.log(error);
-  if(error.code==1){
+    if(error.name== 'SequelizeValidationError' || error.name== 'SequelizeUniqueConstraintError'){
+      res.status(400).json({
+          message: error.errors[0].message
+      })
+    }else if(error.name== 'TypeError'){
+      res.status(400).json({
+          message: 'Invalid username/password'
+      })
+    }else if(error.code==1){
     res.status(401).json({
         message: 'Invalid username/password'
     })
-  }else if(error.name=='SequelizeValidationError'){
-    res.status(400).json({
-        message: error.errors[0].message
-    })
-  }else{
+    }else if(error.code==2){
+      res.status(404).json({
+          message: 'Product not found'
+      })
+      }else{
     res.status(500).json({
         message: 'Internal server error'
     })
