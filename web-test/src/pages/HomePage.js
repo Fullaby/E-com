f55efcc46cb020a898f55bb736194de8de616617
steven-leonardo <stevenleonardolim@gmail.com>
2022-09-27 09:26:37 +0700

    NavBar fix

--- web-test/src/pages/HomePage.js
+++ web-test/src/pages/HomePage.js
@@ -1,9 +1,11 @@
+import CarouselHome from "../components/CarouselHome";
 import NavBar from "../components/NavBar";
 
 export default function HomePage(){
     return(
         <div className="">
             <NavBar/>
+            <CarouselHome/>
             <h1>test</h1>
         </div>
     )
