ca15762446357b50985ceeb2523e0fd6e3d3a3f1
steven-leonardo <stevenleonardolim@gmail.com>
2022-10-02 21:27:07 +0700

    card for products

---
+++ web-test/src/components/CardItem.js
@@ -0,0 +1,16 @@
+export default function CardItem(){
+    return(
+        <a href="#">
+<div style={{width:'200px'}} className=" bg-white rounded-lg border border-gray-200 shadow-md ml-10">
+        <img className="rounded-t-lg" src="https://api.watsons.co.id/medias/SM-TOLAK-ANGIN-CAIR-5S-14250.jpg?context=bWFzdGVyfGZyb250L3pvb218NjEyMjY3fGltYWdlL2pwZWd8ZnJvbnQvem9vbS84ODk2NTA3MjgxNDM4LmpwZ3xkZjMwM2FhMTUzOGU2MjUzNjg0OTA5ODU4ZTI2ZTBjNGY1ZDZkMmFhNDFmNjczM2RlMjcwOTEyNjM1NGRmY2Jk" alt=""/>
+    <div className="p-5">
+        <div>
+        <p className="mb-1 font-normal whitespace-nowrap overflow-hidden text-ellipsis" style={{fontSize: '10pt'}}>Tolak Angin Bebas Gula Cair Dus 3x5's Herbal - Masuk </p>
+        <p className=" font-bold">Rp. 1000</p>
+        </div>
+    </div>
+</div>
+</a>
+
+    )
+}
